9e655613afba09c772268c7dde6700d9c913a49c
victor <boredtodeath555@gmail.com>
2026-05-27 14:30:25 -0400

    click anywhere to continue text

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,8 +1,4 @@
-#extends Control
-#
-#@onready var dialogue_log: RichTextLabel = $Panel/VBoxContainer/DialogueLog
-#@onready var choice_container: VBoxContainer = $Panel/VBoxContainer/ChoiceContainer
-#
+
 #func _ready():
 	## Connect to Dialogic timeline events
 	#Dialogic.timeline_ended.connect(_on_timeline_ended)
